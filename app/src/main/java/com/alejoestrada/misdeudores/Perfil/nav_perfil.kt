5baa95c20a4156e65f4933096db7bde0cb8381f6
Jei Ortiz <ortizjeison4@gmail.com>
2020-12-15 23:00:51 -0500

    recetas

--- app/src/main/java/com/alejoestrada/misdeudores/Perfil/nav_perfil.kt
+++ app/src/main/java/com/alejoestrada/misdeudores/Perfil/nav_perfil.kt
@@ -30,7 +30,7 @@ class nav_perfil : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false)
     }
-
+ // hola
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentPerfilBinding.bind(view)
 
